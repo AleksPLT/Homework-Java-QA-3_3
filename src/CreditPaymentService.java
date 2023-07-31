ca0e9a9eabc604aef18f0b37bbd708cc3e3ae754
Aleksandr Plotnikov <legendpro93@gmail.com>
2023-07-31 18:34:11 +0300

    Change CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-    public int calculate(int a, int b){
+    public int calculate(int sum, int years){
         double percent = 9.99;
-        int months =(b * 12); // кол-во месяцев в зависимости от количества выбраных лет
+        int months =(years * 12); // кол-во месяцев в зависимости от количества выбраных лет
         double monthPercent = (percent/(100 * 12));
         double coefficient = (monthPercent * ((Math.pow((1 + monthPercent) , months)) / ((Math.pow((1 + monthPercent) , months)) - 1))); //коэффициент аннуитета
-        double result = (a * coefficient);
+        double result = (sum * coefficient);
 
         return (int)result;
     }
